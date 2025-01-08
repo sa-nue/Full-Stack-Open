e8c42ca8bb4cbee0ce91e17a699fe97dab546c39
sa-nue <lodenberg@protonmail.com>
2025-01-08 17:55:25 +0100

    Update App.jsx

--- part1/courseinfo/App.jsx
+++ part1/courseinfo/App.jsx
@@ -1,7 +1,7 @@
 const Header = (course) => {
     console.log(course)
     return (
-        <h1>{course.course}</h1>
+        <h1>{course.course.name}</h1>
     )
 }
 
@@ -29,26 +29,37 @@ const Total = (total) => {
     console.log(total)
     return (
         <p>
-            Number of exercises {total.exercises1 + total.exercises2 + total.exercises3}
+            Number of exercises {total.total[0].exercises + total.total[1].exercises + total.total[2].exercises}
         </p>
     )
 }
 
+export default App
+
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-    const parts = [{ name: part1, exercises: exercises1 }, { name: part2, exercises: exercises2 }, { name: part3, exercises: exercises2 }]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
             <Header course={course} />
-            <Content content={parts} />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Content content={course.parts} />
+            <Total total={course.parts} />
         </div>
     )
 }
