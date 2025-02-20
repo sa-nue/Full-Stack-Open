ddcdd5667db8efeeacd5e731a6a3132391a883e6
sa-nue <lodenberg@protonmail.com>
2025-02-20 15:02:13 +0100

    Update index.js

--- render-test/index.js
+++ render-test/index.js
@@ -9,10 +9,6 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/get-ip', (req, res) => {
-    res.send({ ip: req.ip })
-})
-
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
